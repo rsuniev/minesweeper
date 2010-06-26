f5e5449d7a27f5d5c0cab003841ea7f32e6b5994
rsuniev <rustem.suniev@gmail.com>
2010-06-26 23:06:09 +0100

    A board is now correctly updated. Command test is still failing.

--- src/minesweeper/core.clj
+++ src/minesweeper/core.clj
@@ -3,43 +3,24 @@
 
 (def *board* (ref []))
 
-;(def board (ref []))
-
 (defn create-board [n m]
   (dosync
    (ref-set *board*
           (vec (flatten
                 (for [x (range n) y (range m)] {:x x :y y :mine false}))))))
  
-;(defn create-board [n m]
-;  (vec (flatten (for [x (range n) y (range m)] {:x x :y y :mine false}))))
- 
-;(defn find-cell [x y]
-;  (some #(when (= (map % [:x :y]) [x y]) %) board))
+(defn place-mine [x y]
+  (dosync
+   (alter *board*
+          (partial clojure.walk/walk
+                   #(if (= (map % [:x :y]) [x y])
+                      (assoc % :mine true) %) identity))))
 
-(defn place-mine [line-num mine-position]
-  (assoc (find-cell mine-position line-num) :mine true))
+(defn mine-positions [mines-str]
+  (positions #(= \* %) (seq mines-str)))
 
-
-;(defn add-mine [board line-num mine-position]
-;  (let [updated-cell
-;        (assoc
-;            (some #(when (= (map % [:x :y])
-;                            [mine-position line-num]) %) board) :mine true)]
-;    (conj board updated-cell)))
-
-;(defn remove-mine [board x y]
-;  (remove #(= (map % [:x :y :mine]) [x y false]) board))
- 
-;(defn mine-to-board [y x]
-;  (alter-var-root #'board add-mine y x)
-;  (alter-var-root #'board remove-mine x y))
-
-;(defn mine-positions [mines-str]
-;  (positions #(= \* %) (seq mines-str)))
-
-;(defn apply-mine-command [command]
-;    (let [mines (map #(mine-positions %) (.split command "\n"))]
-;      (for [i (range (count mines))]
-;        (map (partial mine-to-board i) (nth mines i)))
-;      board))
+(defn apply-mine-command [command]
+    (let [mines (map #(mine-positions %) (.split command "\n"))]
+      (for [i (range (count mines))]
+        (map (partial place-mine i) (nth mines i)))
+      @*board*))
--- test/minesweeper/core_test.clj
+++ test/minesweeper/core_test.clj
@@ -10,74 +10,34 @@
 ;  ((create-board 2 2)
 ;   (f)))
 
+;(defn board-fixture [f]
+;  (binding [test-board (create-board 2 2)]
+;    (f)))
+
 (defn board-fixture [f]
-  (binding [*board* (create-board 2 2)]
-    (f)))
+  (create-board 2 2)
+  (f))
 
 (use-fixtures :each board-fixture)
 
-;(defmacro def-board-test [name & body]
-;  `(deftest ~name
-;     (binding [*board* (create-board 2 2)]
-;       ~@body)))
-
 (deftest test-create-board
-  (is (= *board* [{:x 0 :y 0 :mine false}
+  (is (= @*board* [{:x 0 :y 0 :mine false}
                   {:x 0 :y 1 :mine false}
                   {:x 1 :y 0 :mine false}
                   {:x 1 :y 1 :mine false}])))
 
-(deftest test-create-board-2
-  (is (= *board* [{:x 0 :y 0 :mine false}
-                  {:x 0 :y 1 :mine false}
-                  {:x 1 :y 0 :mine false}
-                  {:x 1 :y 1 :mine false}])))
+(deftest test-place-mine
+  (is (= (place-mine 0 1) [{:x 0 :y 0 :mine false}
+                   {:x 0 :y 1 :mine true}
+                   {:x 1 :y 0 :mine false}
+                   {:x 1 :y 1 :mine false}])))
+                                        
+(deftest test-mine-positions
+  (is (= '(0 2) (mine-positions "*.*.")))
+  (is (= '() (mine-positions "...."))))
 
-;(deftest test-generate-board-2
-;  (is (= [{:x 0 :y 0 :mine false}
-;          {:x 0 :y 1 :mine false}
-;          {:x 1 :y 0 :mine false}
-;          {:x 1 :y 1 :mine false}] test-board)))
-
-;(load-board 2 2)
-
-;(defn clean-board [board]
-;  (board []))
-
-;(defn boards-fixture [f]
-;  (load-board 2 2)(f))
-
-
-
-;(deftest test-find-cell
-;  (is (= {:x 1 :y 0 :mine false} (find-cell 1 0)))
-;  (is (= {:x 1 :y 1 :mine false} (find-cell 1 1))))
-
-;(deftest test-place-mine
-;  (is (= {:x 1 :y 0 :mine true} (place-mine 0 1))))
-
-;(deftest test-load-board2
-;  (is (= [{:x 0 :y 0 :mine false}
-;          {:x 0 :y 1 :mine false}
-;          {:x 1 :y 0 :mine false}
-;          {:x 1 :y 1 :mine false}] board)))
-
-;(deftest test-mine-positions
-;  (is (= '(0 2) (mine-positions "*.*."))))
-
-
-;(deftest test-placing-mine-via-command
-;  (is (= ".." (apply-mine-command ".*\n*.\n"))))
-
-;(deftest test-mine-to-board
-;  (mine-to-board 1 1)
-;  (is (= [{:x 0 :y 0 :mine false}
-;          {:x 0 :y 1 :mine false}
-;          {:x 1 :y 0 :mine false}
-;          {:x 1 :y 1 :mine true}] board)))
-
-;(deftest test-placing-mine-via-command    
-;  (is (= [{:x 0 :y 0 :mine false}
-;          {:x 0 :y 1 :mine true}
-;          {:x 1 :y 0 :mine true}
-;          {:x 1 :y 1 :mine false}] (apply-mine-command ".*\n*.\n"))))
+(deftest test-placing-mine-via-command    
+  (is (= [{:x 0 :y 0 :mine false}
+          {:x 0 :y 1 :mine true}
+          {:x 1 :y 0 :mine true}
+          {:x 1 :y 1 :mine false}] (apply-mine-command ".*\n*.\n"))))
